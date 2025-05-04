a2ed9c3d13ca14afdc00d550c0c235ba153600e9
Tomology12 <P2782523@my365.dmu.ac.uk>
2025-05-04 21:49:03 +0100

    first push
    .

---
+++ Build/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Tom Beard/Documents/GitHub/Assignement_24_25-main/Build/_deps/googletest-src
+# Build directory: C:/Users/Tom Beard/Documents/GitHub/Assignement_24_25-main/Build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
